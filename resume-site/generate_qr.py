4b22ad7bc107770c8689a447b0730b3f23ca8307
jeevan patidar <bhaveshpatidar8827@gmail.com>
2025-07-24 19:01:14 +0530

    new line

---
+++ resume-site/generate_qr.py
@@ -0,0 +1,18 @@
+import qrcode
+from PIL import Image
+
+# जब आप GitHub Pages पर host कर लेंगे, इस link को actual page से replace करें
+data = "https://your-username.github.io/resume-site/"
+
+qr = qrcode.QRCode(
+    version=2,
+    box_size=10,
+    border=4
+)
+
+qr.add_data(data)
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("resume_qr.png")
+Image.open("resume_qr.png").show()
